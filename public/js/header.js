ff0ddab3799db4aa946f7c7caacebc470fd1a238
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2021-02-28 18:40:52 +0100

    Update header.js

--- public/js/header.js
+++ public/js/header.js
@@ -5,14 +5,14 @@ $(function(){
     $('form.global-search .search').click(function(event){
         event.preventDefault();
         var page = $(this).parent().find('input[type="text"]').val(),
-            redirect = '9tp9.github.io';
+            redirect = '/';
         
         switch(true){
-            case /^(trofeos|trophies)$/.test(page): redirect = '9tp9.github.io/trofeos/'; break;
-            case /^(csgo|cs|counter)$/.test(page): redirect = '9tp9.github.io/trofeos/csgo/'; break;
-            case /^(rocket league|rocket)$/.test(page): redirect = '9tp9.github.io/trofeos/rocket-league/'; break;
-            case /^(sobre nosotros|9tp9|quienes somos|who)$/.test(page): redirect = '9tp9.github.io/who/'; break;
-            case /^(social|redes|redes sociales)$/.test(page): redirect = '9tp9.github.io/social/'; break;
+            case /^(trofeos|trophies)$/.test(page): redirect = '/trofeos/'; break;
+            case /^(csgo|cs|counter)$/.test(page): redirect = '/trofeos/csgo/'; break;
+            case /^(rocket league|rocket)$/.test(page): redirect = '/trofeos/rocket-league/'; break;
+            case /^(sobre nosotros|9tp9|quienes somos|who)$/.test(page): redirect = '/who/'; break;
+            case /^(social|redes|redes sociales)$/.test(page): redirect = '/social/'; break;
             default: break;
         }
 
