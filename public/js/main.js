c9f6c8a9e150ae80da9facabd7dc739f0b267300
csegundo <carlossn20165@gmail.com>
2021-03-01 23:09:49 +0100

    Empezando Owl Carousel

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,25 @@ $(function(){
         var game = $(this).data().name;
         window.location.href = `9tp9.github.io/trofeos/${game}/`;
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop            : true,
+        margin          : 10,
+        responsiveClass : true,
+        responsive      : {
+            0 : {
+                items : 1,
+                nav : true
+            },
+            600 : {
+                items : 3,
+                nav : false
+            },
+            1000 : {
+                items : 5,
+                nav : true,
+                loop : false
+            }
+        }
+    }); 
 });
